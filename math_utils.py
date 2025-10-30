c654d31290fd33900ba5b64544cfefb90932f284
noammelusyan-del <noammelusyan@gmail.com>
2025-10-30 13:47:37 +0200

    Update math_utils.py

--- math_utils.py
+++ math_utils.py
@@ -1,9 +1,7 @@
 def find_max_number(num1, num2, num3):
-    if num1 < num2:
-     return num2
-    elif num2 < num3:
-     return num3 
-    elif num1 < num3:
+    if num1 >= num2 and num1 >= num3
+     return num1
+    elif num2 =< num3 and num1 =< num3
      return num3 
     else:
       return num3
